b5ed154aff4ba710b199eac9bf51b121f361509e
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-17 22:22:54 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -35,8 +35,7 @@ class Server:
         return (start_index, end_index)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """get page function
-        """
+        """get page function"""
         assert type(page) == int or type(page_size) == int
         assert page > 0 or page_size > 0
 
@@ -50,3 +49,24 @@ class Server:
             return list_result
 
         return data[start:end]
+
+
+def get_hyper(self, page: int, page_size: int) -> dict:
+        """
+        Hypermedia pagination
+        """
+        data = self.get_page(page, page_size)
+        size_all_pages = math.ceil(len(self.get_dataset()) / page_size)
+        next_page = page + 1 if page + 1 < size_all_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        hyper_data = {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": size_all_pages,
+        }
+
+        return hyper_data
