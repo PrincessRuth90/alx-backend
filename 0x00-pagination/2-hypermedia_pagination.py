e16d85bb1d1a4d3d7e7cf010b07fc34ba599afdf
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-18 00:01:00 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -27,7 +27,9 @@ class Server:
         return self.__dataset
 
     def index_range(self, page: int, page_size: int) -> tuple:
-        """return a tuple of size two containing a start index and an end index"""
+        """
+        return a tuple of size two containing a start index and an end index
+        """
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
         return (start_index, end_index)
