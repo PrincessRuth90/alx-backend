06b1f9db4d2d0c0d2a99ca927e7d7657c1f9eb02
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-19 07:54:46 +0000

    FIFO caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,9 +9,11 @@ class BasicCache(BaseCaching):
     """BasicCache"""
 
     def put(self, key, item):
+        """put function"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """get function"""
         if key:
             return self.cache_data.get(key, None)
