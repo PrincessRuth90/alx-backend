acf5f0a38d1998406ce4f06cf4dea0683ac62903
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-19 07:35:12 +0000

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class BasicCache that inherits from BaseCaching and is a caching system"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache"""
+
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key:
+            return self.cache_data.get(key, None)
